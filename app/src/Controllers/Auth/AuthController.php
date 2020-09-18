0881470638aa264de1cd4ca329c2c01b5ce9af9a
Jonathan Bailon Segura <jonn59@gmail.com>
2020-09-17 19:13:05 -0500

    Agregamos logs al cerrar e iniciar sesion

--- app/src/Controllers/Auth/AuthController.php
+++ app/src/Controllers/Auth/AuthController.php
@@ -5,11 +5,13 @@ namespace App\Controllers\Auth;
 //use App\Models\User;
 use App\Controllers\BaseController;
 use Respect\Validation\Validator as v;
+use App\Models\Logs;
 
 class AuthController extends BaseController {
 
 	public function getSignOut($request, $response) {
-		$this->auth->logout();
+		Logs::registra_log( $this->session->id, 'Logout', '/' );
+        $this->auth->logout();
 		return $response->withHeader('Location', $this->router->urlFor('home'));
 	}
 
@@ -25,6 +27,8 @@ class AuthController extends BaseController {
 			$this->flash->addMessage('error', 'Tu usuario o contraseña son incorrectos');
             return $response->withHeader('Location', $this->router->urlFor('home'));
 		}
+
+        Logs::registra_log( $this->session->id, 'Login', '/' );
 
         return $response->withHeader('Location', $this->router->urlFor('niveles'));
 	}
